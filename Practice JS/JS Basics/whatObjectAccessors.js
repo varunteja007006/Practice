b9406eec26ff63cf8200b985db14ba87eb6c053e
Varun <varunteja007006@gmail.com>
2023-11-14 16:15:19 +0530

    Practice JS

---
+++ Practice JS/JS Basics/whatObjectAccessors.js
@@ -0,0 +1,16 @@
+/*
+
+Why use getters and setters?
+
+It gives simpler syntax
+It allows equal syntax for properties and methods
+It can secure better data quality
+It is useful for doing things behind-the-scenes
+*/
+
+
+// setter Function
+
+// getter Function
+
+// Function or getter as property, which one to choose?
---
+++ Practice JS/JS Basics/whatOperaters.js
@@ -0,0 +1,43 @@
+/*
+Arithmetic Operators
+Assignment Operators
+Comparison Operators
+String Operators
+Logical Operators
+Bitwise Operators
+Ternary Operators
+Type Operators
+*/
+
+/*
+
+Arithmetic Operations
++ addition
+- substraction
+* multiplication
+** exponential
+/ division
+% modulo
+++ increment
+-- decrement
+
+*/ 
+
+
+// Ternary operator
+const data = ['hello']
+console.log('Ternary Operator');
+data?console.log('yes'):console.log("no");
+
+// Loose Comparision
+let a = 1
+let b = '1'
+console.log('Loose Comparision');
+
+if ( a == b) {
+    console.log('yes');
+}
+else{
+    console.log('no');
+}
+
