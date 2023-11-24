a0af4301725fb70a0c27b9ebdf0b3222ece6a2e0
varun <varunteja007006@gmail.com>
2023-11-24 17:51:41 +0530

    Updated Practice TS

---
+++ Practice TS/sample.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+    TypeScript code here
+    1. Code in TypeScript
+    2. Compile to JavaScript -> tsc sample.js
+    3. Run the JavaScript file using node -> node sample.js
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
---
+++ Practice TS/sample.ts
@@ -0,0 +1,8 @@
+/* 
+    TypeScript code here
+    1. Code in TypeScript
+    2. Compile to JavaScript -> tsc sample.js
+    3. Run the JavaScript file using node -> node sample.js
+*/
+
+export {};
