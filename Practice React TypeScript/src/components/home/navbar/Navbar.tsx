c86336cac3b316ff3a6c4fe4a44450e9adb4a4a0
varun <varunteja007006@gmail.com>
2023-11-30 12:10:17 +0530

    Updated React TS

--- Practice React TypeScript/src/components/home/navbar/Navbar.tsx
+++ Practice React TypeScript/src/components/home/navbar/Navbar.tsx
@@ -8,10 +8,38 @@ import {
   NavigationMenuTrigger,
   //   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
+import { NavLink } from "react-router-dom";
 
-function Navbar(){
+function Navbar() {
   return (
-    <NavigationMenu>
+    <NavigationMenu className="p-5 flex">
+      {/* brand  */}
+      <NavigationMenuList>
+        <NavigationMenuItem className="text-2xl me-5">
+          <NavLink to={"/"}>React 💙 </NavLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+
+      <NavigationMenuList className="justify-center">
+        <NavigationMenuItem>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              [isActive ? "active bg-red-500" : "bg-yellow-400"].join(" ")
+            }
+          >
+            Home
+          </NavLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavLink
+            to={"/aboutme"}
+            className={"bg-yellow-400 px-3 py-2 text-black rounded-lg"}
+          >
+            About Me
+          </NavLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
       <NavigationMenuList>
         <NavigationMenuItem>
           <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
