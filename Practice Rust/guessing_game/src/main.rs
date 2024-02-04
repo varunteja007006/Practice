3e9f9c80ea3c960903e72e746e0ec03844c206e5
varunteja <varunteja007006@gmail.com>
2024-02-04 16:54:24 +0530

    Updated guessing_game

--- Practice Rust/guessing_game/src/main.rs
+++ Practice Rust/guessing_game/src/main.rs
@@ -7,37 +7,60 @@ use std::io; // The io library comes from the standard library, known as std. Th
 // The ones not available in prelude as to be explicitly called out using 'use'
 use rand::Rng;
 
+use std::cmp::Ordering;
+// Ordering gives us 3 variants - Less, Greater, and Equal
+
+
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
     // gen_range method takes a range expression as an argument, start..=end
-    println!("The secret number is: {secret_number}");
-    println!("Plz input your guess!");
+    // println!("The secret number is: {secret_number}");
+    
+    loop{ // loop keyword creates an infinite loop
+        println!("Plz input your guess!");
 
-    let mut guess = String::new(); // we use let in Rust to create a variable. By default they are 
-    // immutable.
+        let mut guess = String::new(); // we use let in Rust to create a variable. By default they are 
+        // immutable.
 
-    // To make a variable mutable we use 'mut'
-    // let apples = 5; // immutable
-    // let mut bananas = 5; // mutable
+        // To make a variable mutable we use 'mut'
+        // let apples = 5; // immutable
+        // let mut bananas = 5; // mutable
 
-    // The :: syntax in the ::new line indicates that new is an associated function of the String type. 
-    // An associated function is a function that’s implemented on a type,
+        // The :: syntax in the ::new line indicates that new is an associated function of the String type. 
+        // An associated function is a function that’s implemented on a type,
 
 
-    io::stdin() // can also be written as follows std::io::stdin
-    .read_line(&mut guess) // read_line takes the variable as argument to store whatever the user 
-    // inputs/types.
+        io::stdin() // can also be written as follows std::io::stdin
+        .read_line(&mut guess) // read_line takes the variable as argument to store whatever the user 
+        // inputs/types.
 
-    // & indicates that this argument is a reference, references are also immutable by default.
-    // Hence we need to mention mut to make it mutable
+        // & indicates that this argument is a reference, references are also immutable by default.
+        // Hence we need to mention mut to make it mutable
 
-    // the above read_line also returns Result (an enumeration having various states called as variants)
-    // Two such variants are `Ok` and `Err`
-    .expect("Failed to read the line");
+        // the above read_line also returns Result (an enumeration having various states called as variants)
+        // Two such variants are `Ok` and `Err`
+        .expect("Failed to read the line");
 
-    // When printing the value of a variable, the variable name can go inside the curly brackets `{}`.
-    println!("You guessed: {guess}");
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        // We create a variable named guess. How is this possible???
+        // Rust allows us to shadow the previous value of guess with a new one. Shadowing lets us reuse 
+        // the guess variable name rather than forcing us to create two unique variables, such as 
+        // guess_str and guess
+
+        // When printing the value of a variable, the variable name can go inside the curly brackets `{}`.
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {println!("You win!"); break;}
+        }
+    }
 }
 
 //When printing the value of a variable, the variable name can go inside the curly brackets.
