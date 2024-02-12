44255020c52d2be5e435a9a26bae3aacebba7c6f
varunteja <varunteja007006@gmail.com>
2024-02-12 23:01:15 +0530

    Practice RUST

---
+++ Practice Rust/rust_ownership/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Welcome to Ownership in Rust");
+}
+// RUST Ownership concept
+
+// It enables Rust to make memory safety guarantees without needing a garbage collector.
+// Some features to be covered:
+// 1. borrowing
+// 2. slices
+
+// Rust's approach to Memory: memory is managed through a system of ownership with a set of 
+// rules that the compiler checks.f any of the rules are violated, the program won’t compile. 
+
+
+// STACK vs HEAP
