f6d3269cd849159f737dbd4cf8393f3cf1eb2bf5
varunteja <varunteja007006@gmail.com>
2024-02-13 23:08:22 +0530

    practice rust_ownership

--- Practice Rust/rust_ownership/src/main.rs
+++ Practice Rust/rust_ownership/src/main.rs
@@ -13,3 +13,25 @@ fn main() {
 
 
 // STACK vs HEAP
+// Stack & Heap are parts of memory available at runtime but structured differently.
+// Stack works as last in first out. All data stored in stack must have fixed size.
+// Data with an unknown size at compile time or a size that might change must be stored on the heap instead.
+
+// The heap is less organized: when you put data on the heap, you request a certain amount of space. 
+// The memory allocator finds an empty spot in the heap that is big enough, marks it as being in use, 
+// and returns a pointer, which is the address of that location. This process is called allocating on 
+// the heap and is sometimes abbreviated as just allocating (pushing values onto the stack is not 
+// considered allocating). Because the pointer to the heap is a known, fixed size, you can store the 
+// pointer on the stack, but when you want the actual data, you must follow the pointer.
+
+// Pushing to the stack is faster than allocating on the heap because the allocator never has 
+// to search for a place to store new data; that location is always at the top of the stack.
+
+// Accessing data in the heap is slower than accessing data on the stack because you have to 
+// follow a pointer to get there.
+
+
+// RUST OWNERSHIP RULES
+// * Each value in rust has a owner.
+// * There can be only 1 owner at a time.
+// * When an owner goes out of scope, the value is dropped.
