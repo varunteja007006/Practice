8c0ad2e14882d24ec4cd259553b03bf81e7c9552
varunteja <varunteja007006@gmail.com>
2024-03-07 22:35:29 +0530

    Update

--- Practice Rust/rust_structs/src/main.rs
+++ Practice Rust/rust_structs/src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
     println!("Welcome to Rust Structs!");
+    // Mutable
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
 }
 
 // A struct, or structure, is a custom data type that lets you package together and
@@ -13,3 +22,33 @@ fn main() {
 // email: string,
 // sign_in_count: u64,
 //}
+
+// In other words, the struct definition is like a general template for the type, and instances
+// fill in that template with particular data to create values of the type.
+// fn main() {
+//     let user1 = User {
+//         active: true,
+//         username: String::from("someusername123"),
+//         email: String::from("someone@example.com"),
+//         sign_in_count: 1,
+//     };
+// }
+
+// This below function creates an instance of User
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+// The above code can also be written as below
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
