f3eebf3106d55e0dae394127c2ffac581c93f118
varun <varunteja007006@gmail.com>
2024-03-25 22:19:55 +0530

    update

--- Practice JS/sample.js
+++ Practice JS/sample.js
@@ -1,3 +1,23 @@
-const str = "varun";
-const revStr = str.split("").reverse().join("");
-console.log(revStr);
+// Reverse a string
+// const str = "varun";
+// const revStr = str.split("").reverse().join("");
+// console.log(revStr);
+
+// Using Array.from()
+// Example 1: Creating an array from a string
+const str = "hello";
+const array1 = Array.from(str);
+console.log(array1); // Output: ['h', 'e', 'l', 'l', 'o']
+
+// Example 2: Creating an array from a Set
+const set = new Set([1, 2, 3]);
+const array2 = Array.from(set, (x) => x * 2);
+console.log(array2); // Output: [2, 4, 6]
+
+// Example 3: Using the map function and thisArg
+const mapFn = function (element) {
+  return element * this.multiplier;
+};
+const thisArg = { multiplier: 2 };
+const array3 = Array.from([1, 2, 3], mapFn, thisArg);
+console.log(array3); // Output: [2, 4, 6]
