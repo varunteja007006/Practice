56288e05796aadb2df2a5a9f22cfeb5554f73f57
varun <varunteja007006@gmail.com>
2024-07-14 17:37:15 +0530

    remove the react

--- Practice React/src/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counterSlice";
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer, // we access "counter" by using useSelector hook hence this naming is essential
-  },
-});
--- practice_nextjs/app/(root)/error.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-function Error() {
-  return <div>Error</div>;
-}
-
-export default Error;
